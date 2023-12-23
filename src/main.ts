7edfe837b82182c507a26a5c065357cb9861c086
James Hibberd <james.d.hibberd@gmail.com>
2023-12-23 17:39:52 +0800

    simple logic for generating html for article markdown

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log("hello");
+/**
+ * Generate the static blog site.
+ */
+
+import fs from "fs";
+import path from "path";
+import showdown from "showdown";
+
+const ARTICLE_DIR = "./article";
+const OUTPUT_DIR = "./dist";
+
+fs.rmSync(OUTPUT_DIR, { recursive: true, force: true });
+fs.mkdirSync(OUTPUT_DIR);
+
+const converter = new showdown.Converter();
+fs.readdirSync(ARTICLE_DIR).forEach((file: string) => {
+  const content = fs.readFileSync(path.join(ARTICLE_DIR, file), "utf-8");
+  const html = converter.makeHtml(content);
+  const filename = `${path.parse(file).name}.html`;
+  fs.writeFileSync(path.join(OUTPUT_DIR, filename), html);
+});
